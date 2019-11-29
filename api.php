7b266c24eb13825e860c11cf0a11976957d9e49d
orbachinujbuk <orbachinujbuk@gmail.com>
2019-11-30 05:27:29 +0600

    asd

--- api.php
+++ api.php
@@ -21,21 +21,20 @@
        header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 
        $post_data = file_get_contents("php://input", true);
-       $data = $post_data;
-       $att_count = substr_count($data, "\n");
-       $att_data = explode("\n", trim($data));
+       $att_count = substr_count($post_data, "\n");
+       $att_data = explode("\n", trim($post_data));
        $encoded_data = '';
-       foreach ($att_data as $single_array) {
-       	$line = explode("\t", trim($single_array));
+       for($i = 0; $i < $att_count; $i++) {
+       	$line = explode("\t", trim($att_data[$i]));
         $encoded_data += json_encode($line);
        }
-       // $line = explode("\t", trim($data));
+       // $line = explode("\t", trim($post_data));
        // $encoded_data = json_encode($att_data);       
        
        $conn = new mysqli("localhost", "killabd_user", "Mannan.KillaBD.123", "killabd_db");
-       $sql ="INSERT INTO attendances (data, sn, count, created_at, updated_at) VALUES ('".$encoded_data."', '". $_GET['SN'] ."', '". substr_count($data, "\n") ."', '".date('Y-m-d H:i:s')."', '".date('Y-m-d H:i:s')."')";
+       $sql ="INSERT INTO attendances (data, sn, count, created_at, updated_at) VALUES ('".$encoded_data."', '". $_GET['SN'] ."', '". substr_count($post_data, "\n") ."', '".date('Y-m-d H:i:s')."', '".date('Y-m-d H:i:s')."')";
        if ($conn->query($sql)===true) {
-           $att_count = substr_count($data, "\n");
+           $att_count = substr_count($post_data, "\n");
        }
        $att_count ='OK: '.$att_count;
        // $conn->close();
